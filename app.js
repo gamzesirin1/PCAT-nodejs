edfb5368318b5b972bc6fb36ec110641343e8cc2
Gamze Şirin <117711013+Gamzesrn@users.noreply.github.com>
2023-09-12 14:29:49 +0300

    static files uploaded.

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-const fn = function () {
-  console.log('fn');
+const express = require('express');
+const path = require('path');
+const app = express();
+const myLogger = (req, res, next) => {
+  console.log('Middleware Log 1');
+  next();
 };
+const myLogger2 = (req, res, next) => {
+  console.log('Middleware Log 2');
+  next();
+};
+app.use(express.static('public'));
+app.use(myLogger);
+app.use(myLogger2);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/temp/index.html');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı.`);
+});
