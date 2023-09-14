22b6c2e22da2b8f3fde1b67993e44d12d4983cb7
Gamze Şirin <117711013+Gamzesrn@users.noreply.github.com>
2023-09-14 13:24:35 +0300

    created uploads fie.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 const esj = require('ejs');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 const Photo = require('./models/Photo');
 const dotenv = require('dotenv');
@@ -20,11 +23,13 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
+
 app.use(myLogger);
 app.use(myLogger2);
 
 app.get('/', async (req, res) => {
-  const photos = await Photo.find({});
+  const photos = await Photo.find({}).sort('-dateCreated');
   res.render('index', {
     photos,
   });
@@ -44,9 +49,26 @@ app.get('/add', (req, res) => {
   res.render('add');
 });
 app.post('/photos', async (req, res) => {
+  // console.log(req.files.image);
   // async - await yapısı kullanacğız.
-  await Photo.create(req.body); // body bilgisini Photo modeli sayesinde veritabanında dökümana dönüştürüyoruz.
-  res.redirect('/');
+  // await Photo.create(req.body); // body bilgisini Photo modeli sayesinde veritabanında dökümana dönüştürüyoruz.
+  // res.redirect('/');
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadImage.name;
+
+  uploadImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadImage.name,
+    });
+    res.redirect('/');
+  });
 });
 
 const PORT = process.env.PORT || 5000;
