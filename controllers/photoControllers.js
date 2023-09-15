fab7b0e5b19dc92b29c6dc822c5056ec0616670c
Gamze Şirin <117711013+Gamzesrn@users.noreply.github.com>
2023-09-15 21:28:54 +0300

    pagination created.

--- controllers/photoControllers.js
+++ controllers/photoControllers.js
@@ -2,10 +2,18 @@ const Photo = require('../models/Photo');
 const fs = require('fs');
 
 exports.getAllPhotos = async (req, res) => {
-  const photos = await Photo.find({}).sort('-dateCreated');
-  res.render('index', {
-    photos,
-  });
+  // const photos = await Photo.find({}).sort('-dateCreated');
+  // res.render('index', {
+  //   photos,
+  // });
+  const page = req.query.page || 1; // Başlangıç sayfamız veya ilk sayfamız.
+  const photosPerPage = 3; // Her sayfada bulunan fotoğraf sayısı
+  const totalPhotos = await Photo.find().countDocuments(); // Toplam fotoğraf sayısı
+
+  const photos = await Photo.find({}) // Fotoğrafları alıyoruz
+    .sort('-dateCreated') // Fotoğrafları sıralıyoruz
+    .skip((page - 1) * photosPerPage) // Her sayfanın kendi fotoğrafları
+    .limit(photosPerPage); // Her sayfada olmasını istediğimi F. sayısını sınırlıyoruz.
 };
 
 exports.getPhoto = async (req, res) => {
